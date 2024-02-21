34f51de52eee51bb1087292f0d4b9e0f98fbf4fa
lantranlan007 <71231001+lantranlan007@users.noreply.github.com>
2024-02-21 22:04:15 +0700

    add redis cache with lettuce

--- SpringBoot/basic/src/main/java/com/example/basic/all/Student.java
+++ SpringBoot/basic/src/main/java/com/example/basic/all/Student.java
@@ -1,6 +1,8 @@
 package com.example.basic.all;
 
-public class Student {
+import java.io.Serializable;
+
+public class Student implements Serializable{
 	private int id;
 	private String name;
 	public Student(int id, String name) {
--- SpringBoot/basic/src/main/java/com/example/basic/all/StudentService.java
+++ SpringBoot/basic/src/main/java/com/example/basic/all/StudentService.java
@@ -2,14 +2,21 @@ package com.example.basic.all;
 
 import java.util.ArrayList;
 
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Service;
 
 @Service
+@EnableCaching
 public class StudentService {
+	
+	@Cacheable("student")
 	public Student getStudentById(int id) {
 		if (id==0) {
+			System.out.println("Hit DB get id 0");
 			return getDummyData().get(0);
 		}
+		System.out.println("Hit DB get id 1");
 		return getDummyData().get(1);
 	}
 	private ArrayList<Student> getDummyData() {
